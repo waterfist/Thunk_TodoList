39c00c022e3f1f5fd04bbf40b69224751e68b79c
hotsix <codinggo@naver.com>
2023-01-27 23:59:47 +0900

    first commit

---
+++ src/redux/components/TodoList/TodoList.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { StyledDiv, StyledTodoListHeader, StyledTodoListBox } from "./styles";
+import Todo from "../Todo";
+import { __getTodos } from "../../modules/todos";
+
+/**
+ * 컴포넌트 개요 : 메인 > TODOLIST. 할 일의 목록을 가지고 있는 컴포넌트
+ * 2022.12.16 : 최초 작성
+ *
+ * @returns TodoList 컴포넌트
+ */
+function TodoList({ isActive }) {
+  const todos = useSelector((state) => state.todos.todos);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(__getTodos());
+  }, []);
+
+  return (
+    <StyledDiv>
+      <StyledTodoListHeader>
+        {isActive ? "해야 할 일 ⛱" : "완료한 일 ✅"}
+      </StyledTodoListHeader>
+      <StyledTodoListBox>
+        {todos
+          ?.filter((item) => item.isDone === !isActive)
+          .map((item) => {
+            {
+              /* console.log("item", item); */
+            }
+            return <Todo key={item.id} todo={item} isActive={isActive} />;
+          })}
+      </StyledTodoListBox>
+    </StyledDiv>
+  );
+}
+
+export default TodoList;
---
+++ src/redux/config/configStore.js
@@ -0,0 +1,10 @@
+import todos from "../modules/todos";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    todos,
+  },
+});
+
+export default store;
